f446454d293c020c1d924531b1f29504f8d10d63
MarcCompton <164903151+MarcCompton@users.noreply.github.com>
2024-11-20 13:18:20 -0600

    yes

---
+++ cyberbullyingProject.java
@@ -0,0 +1,26 @@
+/*
+
+*/
+import Java.util.Scanner;
+
+enum Project {
+    One, Two, Three, Four, Five
+}
+
+public class cyberbullyingProject {
+    public static void main(String[] args) {
+
+    Scanner input = new Scanner (System.in);
+    // Display available options to choose from
+    System.out.println("Please select an option from the following");
+    for(Project project : Project.values()) {
+        System.out.println("- " + Project);
+    } 
+
+    }
+
+    switch(r) {
+        case
+    }
+
+}
