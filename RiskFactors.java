d4212f68470aa4284578f9f0c093a156e7877902
rwheelermusic <rwheeler@stu.jsu.edu>
2024-11-20 12:56:58 -0600

    Changes

--- RiskFactors.java
+++ RiskFactors.java
@@ -1,1 +1,21 @@
 //Ralph
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RiskFactors{
+    public static void main(String[] args) {
+        try {
+            File file1 = new File("RiskFactors.txt");
+            Scanner sc = new Scanner(file1);
+            while(sc.hasNextLine()){
+                    System.out.println(sc.nextLine());
+            }
+        
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured while reading: " + e);
+        }
+    }
+
+}
