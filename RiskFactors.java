b360d299093ddab2fddb30bba836e6bd6de45f78
MAHIN PATEL <135858914+mahin2406@users.noreply.github.com>
2024-11-23 02:22:41 -0600

    RF

--- RiskFactors.java
+++ RiskFactors.java
@@ -19,12 +19,3 @@ public class RiskFactors {
     }
 }
 
-// public class RiskFactors {
-//     public static void displayRiskFactors() {
-//         System.out.println("\n=== Risk Factors ===");
-//         System.out.println("1. Anonymity: Bullies often hide behind anonymous profiles.");
-//         System.out.println("2. Accessibility: Digital platforms allow 24/7 contact, increasing exposure to abuse.");
-//         System.out.println("3. Peer Pressure: Teens may feel pressured to conform to harmful online behavior.");
-//         System.out.println("4. Emotional Impact: Victims may experience anxiety, depression, or low self-esteem.");
-//     }
-// }
